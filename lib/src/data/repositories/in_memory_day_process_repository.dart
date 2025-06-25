2fb3068036694ff56037d91dd4d469a775bef438
Mehmet Enes Körhan <mekrh4n@gmail.com>
2025-06-25 18:49:06 +0300

    day process mock data has been added

--- lib/src/data/repositories/in_memory_day_process_repository.dart
+++ lib/src/data/repositories/in_memory_day_process_repository.dart
@@ -6,10 +6,52 @@ class InMemoryDayProcessRepository implements DayProcessRepository {
   final List<DayProcessInfo> _processes = [
     // Başlangıç mock verisi
     DayProcessInfo(
-      openingTime: '19.04.2025 09:54:18',
-      closingTime: '1.01.0001 00:00:00', // Henüz kapanmamış gibi
-      description: 'İşlem 1 Açıklama',
+      openingTime: '26.04.2025 23:11',
+      closingTime: '00.00.0000 00:00',
+      description: '12 İşlem',
       status: 'Açık',
+    ),
+    DayProcessInfo(
+      openingTime: '25.04.2025 23:11',
+      closingTime: '26.04.2025 09:24',
+      description: '248 İşlem',
+      status: 'Kapalı',
+    ),
+    DayProcessInfo(
+      openingTime: '24.04.2025 23:11',
+      closingTime: '25.04.2025 09:24',
+      description: '198 İşlem',
+      status: 'Kapalı',
+    ),
+    DayProcessInfo(
+      openingTime: '22.04.2025 23:11',
+      closingTime: '23.04.2025 09:24',
+      description: '72 İşlem',
+      status: 'Kapalı',
+    ),
+    DayProcessInfo(
+      openingTime: '21.04.2025 23:11',
+      closingTime: '22.04.2025 09:24',
+      description: '163 İşlem',
+      status: 'Kapalı',
+    ),
+    DayProcessInfo(
+      openingTime: '20.04.2025 23:11',
+      closingTime: '21.04.2025 09:24',
+      description: '91 İşlem',
+      status: 'Kapalı',
+    ),
+    DayProcessInfo(
+      openingTime: '19.04.2025 23:11',
+      closingTime: '20.04.2025 09:24',
+      description: '98 İşlem',
+      status: 'Kapalı',
+    ),
+    DayProcessInfo(
+      openingTime: '23.04.2025 23:11',
+      closingTime: '24.04.2025 09:24',
+      description: '85 İşlem',
+      status: 'Kapalı',
     ),
   ];
 
